00d8b0dede498055d152be84c96b44cd0004ac5f
betultr <betulzx@gmail.com>
2018-12-26 12:23:39 +0300

    step3 comleted

--- repl_comments.py
+++ repl_comments.py
@@ -1,8 +1,20 @@
+#Codes between 2 and 5 lines from https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py
+from hashlib import sha256
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+mypw="asddsa"
+myhsh=create_hash(mypw)
 li=[]
 while True:
-    a=str(input("Enter a comment:"))
-    li.append(a)
-    i=0
-    while i<len(li):
-        print(str(i+1) + "." + li[i])
-        i=i+1
+    c=str(input("Enter your comment:"))
+    p=str(input("Enter your pasword"))
+    hp=create_hash(p)
+    if hp == myhsh:
+        li.append(c)
+        i=0
+        while i<len(li):
+            print(str(i+1) + "." + li[i])
+            i=i+1
+    else:
+        print("I am sorry,i can't let you do that.")
